726d20be8c1c1991d3a48632c77a3c77befa5e1c
Gerald Gmainer <gerald.gmainer@designium.jp>
2024-06-05 22:14:28 +0200

    Integrate Spring Boot project with Kotlin

---
+++ src/main/kotlin/com/gmainer/budget_book_backend/BudgetBookBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.gmainer.budget_book_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BudgetBookBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BudgetBookBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/gmainer/budget_book_backend/BudgetBookBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gmainer.budget_book_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BudgetBookBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
